7993dc39fe234b8770b5d66497db5fdc47f69b06
Cyberfun <shaofanhan@qq.com>
2023-10-24 19:24:38 +0800

    Update agent.py

--- connection_plugins/agent.py
+++ connection_plugins/agent.py
@@ -1,81 +1,71 @@
-#!/usr/bin/python
+from __future__ import (absolute_import, division, print_function)
 
-# Copyright 2015, Jonathan A. Sternberg <jonathansternberg@gmail.com>
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from io import BytesIO
+import requests, json
+__metaclass__ = type
 
-import os
-import requests
-from StringIO import StringIO
-from ansible import errors, utils
-from ansible.callbacks import vvv
-from ansible.constants import p, get_config
+import ansible.constants as C
+from ansible.errors import AnsibleError, AnsibleConnectionFailure
+from ansible.module_utils.six import text_type, binary_type
+from ansible.module_utils._text import to_bytes, to_native, to_text
+from ansible.plugins.connection import ConnectionBase
+from ansible.utils.display import Display
 
-DEFAULT_USE_SSL = get_config(p, 'agent', 'use_ssl', None, False, boolean=True)
-CERTIFICATE = get_config(p, 'agent', 'certificate', None, None)
+display = Display()
 
-class Connection(object):
+class Connection(ConnectionBase):
+    ''' Local based connections '''
 
-    def __init__(self, runner, host, port, user, password, *args, **kwargs):
-        self.runner = runner
-        self.host = host
-        self.port = port or 8700
-        self.user = user
-        self.password = password
-        self.proto = 'http'
-        if DEFAULT_USE_SSL:
-            self.proto = 'https'
-        self.has_pipelining = True
+    transport = 'local'
+    has_pipelining = True
 
-    def _build_url(self, url):
-        return '{proto}://{host}:{port}{url}'.format(proto=self.proto, host=self.host, port=self.port, url=url)
+    def __init__(self, *args, **kwargs):
+        super(Connection, self).__init__(*args, **kwargs)
+        self.cwd = None
 
-    def connect(self):
-        vvv("ESTABLISH CONNECTION FOR USER: %s" % self.user, host=self.host)
-        self.session = requests.Session()
-        self.session.auth = (self.user, self.password)
-        if CERTIFICATE:
-            self.session.cert = os.path.expanduser(CERTIFICATE)
-        self.session.verify = False
+    def _connect(self):
+        ''' connect to the local host; nothing to do here '''
         return self
 
-    def exec_command(self, cmd, tmp_path, sudoable=False, in_data=None, **kwargs):
-        vvv("EXEC %s" % cmd, host=self.host)
-
-        data = {'command': cmd}
-        executable = kwargs.get('executable')
-        if executable is not None:
-            data['executable'] = executable
-
-        if self.runner.become and sudoable:
-            data['become'] = 1
-            if self.runner.become_method:
-                data['becomeMethod'] = self.runner.become_method
+    def exec_command(self, cmd, in_data=None, sudoable=True):
+        addr = self._play_context.remote_addr
+        if isinstance(cmd, (text_type, binary_type)):
+            cmd = to_bytes(cmd)
+        else:
+            cmd = map(to_bytes, cmd)
 
         files = {}
         if in_data:
-            files['stdin'] = StringIO(in_data)
+            files['stdin'] = BytesIO(in_data)
 
-        r = self.session.post(self._build_url('/exec'), data=data, files=files)
-        if r.status_code == 200:
-            data = r.json()
-            return (data['status'], data['stdin'], data['stdout'], data['stderr'])
+        resp = requests.post('http://{}:8700/exec'.format(addr), data={'command': cmd}, files=files)
+        if not resp.ok:
+            raise AnsibleConnectionFailure('Failed to exec command on {}: {}'.format(addr, resp.reason))
 
-        return (255, '', '', r.text)
+        data = resp.json()
+        return data['status'], data['stdout'], data['stderr']
 
     def put_file(self, in_path, out_path):
-        vvv("PUT %s TO %s" % (in_path, out_path), host=self.host)
         with open(in_path, 'rb') as fp:
-            r = self.session.put(self._build_url('/upload'), data={'dest': out_path}, files={'src': fp})
+            remote_addr = self._play_context.remote_addr
+            resp = requests.put('http://{}:8700/upload'.format(remote_addr), data={'dest': out_path}, files={'src': fp})
 
-        if r.status_code != 200:
-            raise errors.AnsibleError("failed to transfer file from %s" % in_path)
+            if not resp.ok:
+                raise AnsibleConnectionFailure('Failed to upload file: {}'.format(resp.reason))
 
     def fetch_file(self, in_path, out_path):
-        vvv("FETCH %s TO %s" % (in_path, out_path), host=self.host)
-        raise errors.AnsibleError("not unimplemented")
+        ''' fetch a file from local to local -- for compatibility '''
+        super(Connection, self).fetch_file(in_path, out_path)
+
+        display.vvv(u"FETCH {0} TO {1}".format(in_path, out_path), host=self._play_context.remote_addr)
+        self.put_file(in_path, out_path)
 
     def close(self):
-        self.session.close()
+        ''' terminate the connection; nothing to do here '''
+        self._connected = False
+
+    def fetch_file(self, in_path, out_path):
+        raise AnsibleError("not unimplemented")
+
+    def close(self):
+        pass
